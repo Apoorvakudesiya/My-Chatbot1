ae1801ae64b86aa58de75d17edff3c3ba3d56354
Apoorva Kudesiya <apoorva.kudesiya@gmail.com>
2024-05-14 22:54:54 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'My Chatbot',
+    version= '0.0.0',
+    author= 'Gaurav Shrivastava',
+    author_email= 'apoorva.kudesiya@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
